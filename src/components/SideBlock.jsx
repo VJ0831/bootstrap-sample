b8fd9c77891e9803c4e8e305adb569720f323332
Vijay Pedda <vukkav@mozy.com>
2016-02-19 23:48:03 -0800

    add files

---
+++ src/components/SideBlock.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var SideBlock = React.createClass({
+    render:function(){
+
+        var panelStyle={
+            background:'black',
+            color:'white'
+        }
+        var titleStyle={
+            margin:0,
+            padding:0
+        }
+
+        if(this.props.color){
+            panelStyle.background = this.props.color;
+        }
+
+
+        return (
+            <div className="panel panel-default">
+                <div style={panelStyle} className="panel-heading">
+                    <span>{this.props.title}</span>
+                    <h2 style={titleStyle}>{this.props.text}</h2>
+                </div>
+                <div className="panel-body" style={{height: '90px'}}>
+                </div>
+            </div>
+        );
+    }
+});
+
+
+module.exports = SideBlock;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var GridManager = require('./components/GridManager.jsx');
+
+ReactDOM.render(<GridManager />, document.getElementById('mainPanel'));
